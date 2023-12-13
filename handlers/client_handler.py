a620c3f0d52464c31155dfc88c898a95bacbb81a
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2023-12-13 23:53:23 +0300

    Add code for cleaning chat

--- handlers/client_handler.py
+++ handlers/client_handler.py
@@ -25,11 +25,14 @@ async def parse_message_handler(message: Message) -> None:
     if len(parser.vk_parse(message.text)) != 0:
         pass
     if len(parser.youtube_parse(message.text)) != 0:
+        await message.delete()
         await message.answer("Выберите подходящее вам качество видео:",
                              reply_markup=inline_keyboards.create_inline_keyboard(message.text).as_markup())
 
 
 @dispatcher.callback_query()
 async def download_youtube_video(callback: CallbackQuery):
+    await callback.message.delete()
+    # await callback.message.answer("Подождите...\nВыполняется загрузка")
     await BOT.send_video(chat_id=callback.from_user.id,
                          video=FSInputFile(youtube_downloader.download_youtube_video(callback)))
